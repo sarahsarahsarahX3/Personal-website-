a38699346a60ffefa3ad54b96f10ce105e1cc794
Sarah <dawsone.sarah@gmail.com>
2026-01-11 21:01:08 -0500

    home

--- app/components/home/HomeContent.tsx
+++ app/components/home/HomeContent.tsx
@@ -4,7 +4,6 @@ import { Hero } from "@/app/components/home/Hero";
 import { SectionTransition } from "@/app/components/home/SectionTransition";
 import { CoreSkillsSection } from "@/app/components/home/CoreSkillsSection";
 import { BioSection } from "@/app/components/home/BioSection";
-import { BrandsSection } from "@/app/components/home/BrandsSection";
 
 export function HomeContent() {
   return (
@@ -13,9 +12,6 @@ export function HomeContent() {
       <BioSection />
       <SectionTransition>
         <CoreSkillsSection />
-      </SectionTransition>
-      <SectionTransition>
-        <BrandsSection />
       </SectionTransition>
       <SectionTransition className="h-screen flex items-center justify-center">
         <h2 className="text-4xl font-display">Selected Work Placeholder</h2>
