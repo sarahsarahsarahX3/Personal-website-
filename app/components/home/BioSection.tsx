d6ed7b22748a072822de4950ddaa93a70e852558
Sarah <dawsone.sarah@gmail.com>
2026-05-15 13:48:41 -0400

    update

--- app/components/home/BioSection.tsx
+++ app/components/home/BioSection.tsx
@@ -248,7 +248,7 @@ export function BioSection() {
 	                  Hello, my name is Sarah Dawson.
 	                </p>
                 <p className="mt-3 text-base md:text-lg tracking-tight text-text-secondary">
-                  I am a marketing and communications professional specializing in content production, management, and strategy.
+                  I'm a marketing and communications professional specializing in content production, management, and strategy.
                 </p>
                 <p
                   data-text="MY TRACK RECORD"
